ee9fa1baaa2f8a107f4f56dfa7535e6cd2612a0a
Mohammed <mohammed@redapesolutions.com>
2018-04-16 01:17:10 +0800

    Now timer is working with Cron like timer scheduler

--- src/pages/schedule/schedule.ts
+++ src/pages/schedule/schedule.ts
@@ -20,22 +20,37 @@ export class SchedulePage {
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
-  playVideos(){
-    //playing video after a certain of time.
-    console.log('You selected the following time to play video:');
-    var hours = (document.getElementById('hoursSelected').value);
-    var minutes = (document.getElementById('minutesSelected').value);
-    var daytime = (document.getElementById('daytimeSelected').value);
-    if (daytime=='PM') {
-        console.log('now playing PM');
+  playVideos() {
+    var daytime:any = <HTMLInputElement>(document.getElementById('daytimeSelected')).value;
+    var hours:any = <HTMLInputElement>(document.getElementById('hoursSelected')).value;
+    var minutes:any =<HTMLInputElement>(document.getElementById('minutesSelected')).value;
+    var hoursPM:any = parseInt(hours) + 12;
+    var rule = new schedule.RecurrenceRule();
+
+
+    // Play video if daytime AM
+    if (daytime=='AM') {
+      console.log('Now daytime set to AM');
+      rule.minute = minutes;
+      rule.hour = hours;
+      schedule.scheduleJob(rule, function(){
+        console.log(rule);
+        var videoPlayer:any = document.getElementById("videoPlayer");
+        videoPlayer.play();
+      });
     }
-    var playtime = hours +':'+ minutes;
-    console.log(playtime);
 
-    // var videoPlayer:any = document.getElementById("videoPlayer");
-    // videoPlayer.play();
+    // Play video if daytime PM
+    if (daytime=='PM') {
+      console.log('Now daytime set to PM');
+      rule.minute = minutes;
+      rule.hour = hoursPM;
+      schedule.scheduleJob(rule, function(){
+        var videoPlayer:any = document.getElementById("videoPlayer");
+        videoPlayer.play();
+      });
+    }
   }
-
   ionViewDidLoad() {
     console.log('ionViewDidLoad SchedulePage');
   }
